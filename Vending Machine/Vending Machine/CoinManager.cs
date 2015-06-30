f95e5c1bc2932eb21d6a6896f00cd92a25c5fc14
James Stumme <james@stumme.net>
2015-06-29 21:24:39 -0500

    Refactored Insert to reduce repeated code.

---
+++ Vending Machine/Vending Machine/CoinManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class CoinManager
+    {
+        private readonly HashSet<Coins> ACCEPTED_COINS = new HashSet<Coins>
+            {
+                Coins.Nickel,
+                Coins.Dime,
+                Coins.Quarter
+            };
+
+        public string CurrentAmount
+        {
+            get { return _currentAmount.ToString("C", CultureInfo.CurrentCulture); }
+        }
+
+        private decimal _currentAmount = (decimal)0.00;
+
+        public void Insert(Coins coin)
+        {
+            if (ACCEPTED_COINS.Contains(coin))
+                _currentAmount = coin.ToDecimal();
+        }
+    }
+}
