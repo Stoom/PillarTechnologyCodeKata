fadb52b3dda7655d0d196f56529678581e1fdbb9
James Stumme <james@stumme.net>
2015-06-29 21:17:09 -0500

    Pass AcceptNickels test.

---
+++ Vending Machine/Vending Machine/CoinsEnum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public enum Coins
+    {
+        Penny = 1,
+        Nickel = 5,
+        Dime = 10,
+        Quarter = 25,
+        FiftyCent = 50,
+        SilverDollar = 100,
+        GoldDollar = 100
+    }
+
+    public static class CoinsExtensions
+    {
+        public static decimal ToDecimal(this Coins value)
+        {
+            return (decimal) value/100;
+        }
+    }
+}
---
+++ Vending Machine/VentingMachine.Test/AcceptCoinsTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachine;
+
+namespace VentingMachine.Test
+{
+    [TestClass]
+    public class AcceptCoinsTest
+    {
+        private CoinManager coinMgr;
+
+        [TestInitialize]
+        public void Init()
+        {
+            coinMgr = new CoinManager();
+        }
+
+        [TestMethod]
+        public void AcceptNickels()
+        {
+            coinMgr.Insert(Coins.Nickel);
+            Assert.AreEqual("$0.05", coinMgr.CurrentAmount);
+        }
+    }
+}
