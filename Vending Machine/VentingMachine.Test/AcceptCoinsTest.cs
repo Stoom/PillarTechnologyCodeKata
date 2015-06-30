cd3a64172a2039807f24b294c0c98af6ad0d8f6d
James Stumme <james@stumme.net>
2015-06-29 21:21:32 -0500

    Passed AcceptQuarters test

--- Vending Machine/VentingMachine.Test/AcceptCoinsTest.cs
+++ Vending Machine/VentingMachine.Test/AcceptCoinsTest.cs
@@ -28,5 +28,12 @@ namespace VentingMachine.Test
             _coinMgr.Insert(Coins.Dime);
             Assert.AreEqual("$0.10", _coinMgr.CurrentAmount);
         }
+
+        [TestMethod]
+        public void AcceptQuarters()
+        {
+            _coinMgr.Insert(Coins.Quarter);
+            Assert.AreEqual("$0.25", _coinMgr.CurrentAmount);
+        }
     }
 }
